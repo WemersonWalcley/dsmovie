b9dbcb9bf180391cb61bef7a3572d1805d61c950
Wemerson Walcley <wemerson.walcley@outlook.com>
2022-03-08 12:21:27 -0300

    Moviecard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,17 @@
+import {ReactComponent as FullStar} from 'assets/img/full_star.svg'
+import {ReactComponent as HalfStar} from 'assets/img/half_star.svg'
+import {ReactComponent as EmptyStar} from 'assets/img/empty_star.svg'
+import './styles.css';
+
+function MovieStars(){
+    return(<div className="dsmovie-stars-container">
+    <FullStar />
+    <FullStar />
+    <FullStar />
+    <HalfStar />
+    <EmptyStar />
+  </div>
+  );
+}
+
+export default MovieStars;
