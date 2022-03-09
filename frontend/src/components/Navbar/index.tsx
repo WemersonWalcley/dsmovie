f13f4e412ad26d18e588aba4f6d2170aba63455a
Wemerson Walcley <wemerson.walcley@outlook.com>
2022-03-09 09:49:54 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
           <a href="https://github.com/WemersonWalcley">
             <div className="dsmovie-contact-container">
               <GitHubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/WemersonWalcley</p>
             </div>
           </a>
         </div>
